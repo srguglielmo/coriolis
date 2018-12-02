85452d7c728ab2b5bb6c894546d6122565025102
Willyb321 <wbwilliam7@gmail.com>
2018-12-03 08:22:41 +1100

    initial vue bootstrap

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  lintOnSave: false,
+  pwa: {
+		name: 'Orbis.zone',
+		themeColor: '#4DBA87',
+		msTileColor: '#000000',
+		appleMobileWebAppCapable: 'yes',
+		appleMobileWebAppStatusBarStyle: 'black',
+
+		workboxPluginMode: 'InjectManifest',
+		workboxOptions: {
+			// swSrc is required in InjectManifest mode.
+			swSrc: 'public/sw.js',
+			swDest: 'service-worker.js'
+			// ...other Workbox options...
+		}
+	},
+	devServer: {
+		proxy: 'http://localhost:3030',
+		disableHostCheck: true,
+		host: '0.0.0.0'
+	}
+};
