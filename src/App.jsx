01e795903e29418a8633583b72bdab43ca9b5cd9
willyb321 <wbwilliam7@gmail.com>
2018-12-08 09:49:32 +1100

    eject, start

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+import Loadable from "react-loadable";
+import Header from './components/Header';
+const LoadableComponent = Loadable({
+  loader: () => import("./pages/Home"),
+  loading: () => <h2>Loading</h2>
+});
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div style={{ minHeight: '100%' }}>
+        <Switch>
+          <Route exact path='/' component={LoadableComponent}/>
+        </Switch>
+        <Header/>
+
+
         <footer>
           <div className="right cap">
             <a href="https://github.com/EDCD/coriolis" target="_blank" rel="noopener noreferrer"
@@ -12,7 +24,8 @@ class App extends Component {
             <br/>
             <a
               href={'https://github.com/EDCD/coriolis/compare/edcd:develop@{' + window.CORIOLIS_DATE + '}...edcd:develop'}
-              target="_blank" rel="noopener noreferrer" title={'Coriolis Commits since' + window.CORIOLIS_DATE}>Commits since last release
+              target="_blank" rel="noopener noreferrer" title={'Coriolis Commits since' + window.CORIOLIS_DATE}>Commits
+              since last release
               ({window.CORIOLIS_DATE})</a>
           </div>
         </footer>
